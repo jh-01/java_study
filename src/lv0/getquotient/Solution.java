5c5b7489d3f8cc38401150e39c3dcae7a6110a5a
jh-01 <were1117@sm.ac.kr>
2025-04-27 23:53:56 +0900

    [PGS] 나이 출력 - 연산

--- src/lv0/getquotient/Solution.java
+++ src/lv0/getquotient/Solution.java
@@ -1,5 +1,9 @@
 package lv0.getquotient;
 
+/*
+몫 구하기
+https://github.com/jh-01/java_study/tree/main/src/lv0/getquotient
+ */
 class Solution {
     public int solution(int num1, int num2) {
         return num1 / num2;
---
+++ src/lv0/yearofbirth/Main.java
@@ -0,0 +1,9 @@
+package lv0.yearofbirth;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.solution(40));
+        System.out.println(solution.solution(23));
+    }
+}
