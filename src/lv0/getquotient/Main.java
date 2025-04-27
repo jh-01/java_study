9d816fcdac660b46b06dc2807010c9482aa59fad
jh-01 <were1117@sm.ac.kr>
2025-04-27 23:21:48 +0900

    [PGS] 몫 구하기 - 연산

---
+++ src/lv0/getquotient/Main.java
@@ -0,0 +1,9 @@
+package lv0.getquotient;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.solution(10, 5));
+        System.out.println(solution.solution(7, 2));
+    }
+}
---
+++ src/lv0/getquotient/Solution.java
@@ -0,0 +1,7 @@
+package lv0.getquotient;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
