1e79b7a56a20dfcdbab2c039f902705ff2d969eb
jh-01 <were1117@sm.ac.kr>
2025-04-28 09:42:38 +0900

    [PGS] 짝수의 합 - 연산

---
+++ src/lv0/evensum/Main.java
@@ -0,0 +1,9 @@
+package lv0.evensum;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.solution(10));
+        System.out.println(solution.solution(4));
+    }
+}
---
+++ src/lv0/evensum/Solution.java
@@ -0,0 +1,11 @@
+package lv0.evensum;
+
+/*
+짝수의 합
+https://school.programmers.co.kr/learn/courses/30/lessons/120831
+ */
+class Solution {
+    public int solution(int n) {
+        return (n / 2) * (n / 2 + 1);
+    }
+}
