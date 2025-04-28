e27e2a40384d792c9e52c1e1b6cb57a245caa12b
jh-01 <were1117@sm.ac.kr>
2025-04-28 09:21:49 +0900

    [PGS] 두 수의 나눗셈 - 연산

---
+++ src/lv0/division/Main.java
@@ -0,0 +1,10 @@
+package lv0.division;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.solution(3, 2));
+        System.out.println(solution.solution(7, 3));
+        System.out.println(solution.solution(1, 16));
+    }
+}
---
+++ src/lv0/division/Solution.java
@@ -0,0 +1,7 @@
+package lv0.division;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return (int)(((double) num1 / num2) * 1000);
+    }
+}
