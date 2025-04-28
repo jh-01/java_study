b95b8beed446c0cecc9ba4a89ebdd6c6d65e22dc
jh-01 <were1117@sm.ac.kr>
2025-04-28 09:44:02 +0900

    [PGS] 배열의 평균값 - 연산, 반복문

---
+++ src/lv0/arrayaverage/Main.java
@@ -0,0 +1,11 @@
+package lv0.arrayaverage;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        int[] arr1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arr2 = {89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
+        System.out.println(solution.solution(arr1));
+        System.out.println(solution.solution(arr2));
+    }
+}
---
+++ src/lv0/arrayaverage/Solution.java
@@ -0,0 +1,11 @@
+package lv0.arrayaverage;
+
+class Solution {
+    public double solution(int[] numbers) {
+        int sum = 0;
+        for(int i : numbers){
+            sum += i;
+        }
+        return (double) sum / numbers.length;
+    }
+}
