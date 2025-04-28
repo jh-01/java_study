97e72ae908f8d7f1b7e71c4552368d2a8b0218c6
jh-01 <were1117@sm.ac.kr>
2025-04-28 09:11:38 +0900

    [PGS] 두 수의 합 구하기 - 연산

---
+++ src/lv0/addition/Main.java
@@ -0,0 +1,9 @@
+package lv0.addition;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.solution(2, 3));
+        System.out.println(solution.solution(100, 2));
+    }
+}
