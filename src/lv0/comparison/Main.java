3a15df946380370d9177a97cd50611f4290e99bb
jh-01 <were1117@sm.ac.kr>
2025-04-28 09:08:23 +0900

    [PGS] 숫자 비교하기 - 조건문

---
+++ src/lv0/comparison/Main.java
@@ -0,0 +1,10 @@
+package lv0.comparison;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.solution(2, 3));
+        System.out.println(solution.solution(11, 11));
+        System.out.println(solution.solution(7, 99));
+    }
+}
---
+++ src/lv0/comparison/Solution.java
@@ -0,0 +1,11 @@
+package lv0.comparison;
+
+/*
+숫자 비교하기
+https://school.programmers.co.kr/learn/courses/30/lessons/120807
+ */
+public class Solution {
+    public int solution(int num1, int num2) {
+        return num1 == num2 ?  1 :  -1;
+    }
+}
