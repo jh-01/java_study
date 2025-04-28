acbd1b3166b6f573f9c7c9a43b3c0cd756122ba1
jh-01 <were1117@sm.ac.kr>
2025-04-28 09:28:14 +0900

    [PGS] 각도기 - 조건문

---
+++ src/lv0/protractor/Main.java
@@ -0,0 +1,10 @@
+package lv0.protractor;
+
+public class Main {
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        System.out.println(solution.solution(70));
+        System.out.println(solution.solution(91));
+        System.out.println(solution.solution(180));
+    }
+}
