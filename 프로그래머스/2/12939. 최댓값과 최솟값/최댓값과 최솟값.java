0bb0065eedb936db5a63713e9cec6fbdd8afc373
Jihyeon Kim <were1117@sm.ac.kr>
2025-08-28 09:29:06 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 13.60 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        int[] nums = Arrays.stream(s.split(" ")).mapToInt(Integer::parseInt).toArray();
+        Arrays.sort(nums);
+        String answer = nums[0] + " " + nums[nums.length - 1];
+        return answer;
+    }
+}
