719ba683615b020e08966247f4cda0aee6817b91
Jihyeon Kim <were1117@sm.ac.kr>
2025-12-15 12:34:26 +0900

    [level 1] Title: K번째수, Time: 0.33 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,12 +2,14 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-        int[] answer = new int[commands.length];
+        int[] result = new int[commands.length];
+        
         for(int i = 0; i < commands.length; i++){
-            int[] newArr = Arrays.copyOfRange(array, commands[i][0] -1, commands[i][1]);
-            Arrays.sort(newArr);
-            answer[i] = newArr[commands[i][2] - 1];
+            int[] temp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(temp);
+            result[i] = temp[commands[i][2] - 1];
         }
-        return answer;
+        
+        return result;
     }
 }
