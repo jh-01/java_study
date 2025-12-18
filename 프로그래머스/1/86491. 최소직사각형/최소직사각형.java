d576e08ceeab7bdce00143fda76fb9ad4ef28ce7
Jihyeon Kim <were1117@sm.ac.kr>
2025-12-18 13:40:49 +0900

    [level 1] Title: 최소직사각형, Time: 1.19 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,32 +1,21 @@
-import java.util.*;
-
 class Solution {
     public int solution(int[][] sizes) {
-        List<Card> cards = new ArrayList<>();
         int maxW = 0;
-        int maxL = 0;
-        for(int i = 0; i < sizes.length; i++){
-            if(sizes[i][0] >= sizes[i][1]){
-                cards.add(new Card(sizes[i][0], sizes[i][1]));
-                if(maxW < sizes[i][0]) maxW = sizes[i][0];
-                if(maxL < sizes[i][1]) maxL = sizes[i][1];
-            }
-            else {
-                cards.add(new Card(sizes[i][1], sizes[i][0]));
-                if(maxW < sizes[i][1]) maxW = sizes[i][1];
-                if(maxL < sizes[i][0]) maxL = sizes[i][0];
-            }
-        }
-        return maxW * maxL;
-    }
-    
-    public class Card {
-        private int width;
-        private int length;
+        int maxH = 0;
         
-        public Card(int width, int length){
-            this.width = width;
-            this.length = length;
+        for(int i = 0; i < sizes.length; i++){
+            if(sizes[i][0] < sizes[i][1]){
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
         }
+        
+        for(int i = 0; i < sizes.length; i++){
+            if(sizes[i][0] > maxW) maxW = sizes[i][0];
+            if(sizes[i][1] > maxH) maxH = sizes[i][1];
+        }
+        
+        return maxW * maxH;
     }
 }
