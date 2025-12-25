f975793e04a57148ea4fc65b00a2731ac6fed1b2
Jihyeon Kim <were1117@sm.ac.kr>
2025-12-25 19:30:21 +0900

    [level 2] Title: 모음 사전, Time: 0.14 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String word) {
+        int answer = 0;
+        int[] nums = {781, 156, 31, 6, 1};
+        Map<Character, Integer> map = new HashMap<>(); 
+        
+        // 문자에 맞는 숫자 저장 
+        map.put('A', 0); 
+        map.put('E', 1); 
+        map.put('I', 2); 
+        map.put('O', 3); 
+        map.put('U', 4);
+        
+        // 몇자리 글자인지 확인
+        int count = word.length();
+        
+        // 계산
+        for(int i = 0; i < count; i++){
+            char c = word.charAt(i);
+            int x = map.get(c);
+            if(x == 0) answer++;
+            else{
+                answer += x * nums[i] + 1;
+            }
+        }
+        
+        return answer;
+    }
+}
