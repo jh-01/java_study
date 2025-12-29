cc1cb307d1830ebb69bfe8f5700040cf9f936e99
Jihyeon Kim <were1117@sm.ac.kr>
2025-12-29 14:04:00 +0900

    [level 1] Title: 체육복, Time: 0.04 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,35 +1,40 @@
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
-        int[] students = new int[n + 1];
+        int[] clothes = new int[n];
         
-        for(int i = 1; i <= n; i++){
-            students[i] = 1;
+        for(int i = 0; i < n; i++){
+            clothes[i] = 1;
         }
         
         for(int i = 0; i < lost.length; i++){
-            students[lost[i]]--;
+            clothes[lost[i] - 1]--;
         }
         
         for(int i = 0; i < reserve.length; i++){
-            students[reserve[i]]++;
+            clothes[reserve[i] - 1]++;
         }
         
-        for(int i = 1; i <= n; i++){
-            if(students[i] > 0) {
+        for(int i = 0; i < n; i++){
+            if(clothes[i] > 0) {
                 answer++;
-                students[i]--;
             } else {
-                if(i > 1 && students[i - 1] > 0) {
-                    answer++;
-                    students[i - 1]--;
+                if(i != 0) {
+                    if(clothes[i - 1] > 1) {
+                        answer++;
+                        clothes[i - 1]--;
+                        continue;
+                    }
                 }
-                else if(i < n && students[i + 1] > 1){
-                    answer++;
-                    students[i + 1]--;
+                if(i < n - 1) {
+                    if(clothes[i + 1] > 1) {
+                        answer++;
+                        clothes[i + 1]--;
+                    }
                 }
             }
         }
+        
         return answer;
     }
 }
