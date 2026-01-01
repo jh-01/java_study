9b1a69db44478ea059fb7291a754ece9d1cbcba5
Jihyeon Kim <were1117@sm.ac.kr>
2026-01-01 12:55:45 +0900

    [level 2] Title: 큰 수 만들기, Time: 35.08 ms, Memory: 87.6 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -2,21 +2,30 @@ import java.util.*;
 
 class Solution {
     public String solution(String number, int k) {
-        StringBuffer sb = new StringBuffer(number);
+        StringBuffer sb = new StringBuffer();
         
-        int index = 0;
-        while(index < sb.length()){
-            int N = sb.length();
-            if(k == 0) break;
-            if(index < N - 1 && sb.charAt(index) < sb.charAt(index + 1)){
-                sb.deleteCharAt(index);
-                index = Math.max(0, index - 1);
+        // int index = 0;
+        // while(index < sb.length()){
+        //     int N = sb.length();
+        //     if(k == 0) break;
+        //     if(index < N - 1 && sb.charAt(index) < sb.charAt(index + 1)){
+        //         sb.deleteCharAt(index);
+        //         index = Math.max(0, index - 1);
+        //         k--;
+        //     } else {
+        //         index++;
+        //     }
+        // }
+        
+        for(char c : number.toCharArray()){
+            while(sb.length() > 0 && k > 0 && sb.charAt(sb.length() - 1) < c){
+                sb.deleteCharAt(sb.length() - 1);
                 k--;
-            } else {
-                index++;
             }
+            sb.append(c);
         }
         
+        // k가 남아있는 경우 뒤에서부터 삭제
         return sb.substring(0, sb.length() - k);
     }
 }
