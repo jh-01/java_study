a7ef79f2855cae0d695efd4630047586fab29d8b
Jihyeon Kim <were1117@sm.ac.kr>
2026-01-05 18:00:41 +0900

    [level 2] Title: 올바른 괄호, Time: 6.00 ms, Memory: 54.4 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     boolean solution(String s) {
         if(s.charAt(0) == ')') return false;
+        if(s.charAt(s.length() - 1) == '(') return false;
         
         int cnt = 0;
         for(int i = 0; i < s.length(); i++){
