03e1b413a5119fc8bdd17b8d276a7e843ce734f3
Jihyeon Kim <were1117@sm.ac.kr>
2026-01-12 14:18:09 +0900

    [level 3] Title: 입국심사, Time: 50.44 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,28 +1,6 @@
-import java.util.*;
-
 class Solution {
     public long solution(int n, int[] times) {
-        long minT = 1;
-        long maxT = (long) Arrays.stream(times).max().getAsInt() * n;
         long answer = 0;
-        
-        while(minT <= maxT){
-            long midT = (minT + maxT) / 2;
-            long sum = 0;
-            for(int t : times){
-                sum += midT / t;
-                if(sum >= n) break;
-            }
-            
-            if(sum >= n) {
-                answer = midT;
-                maxT = midT - 1;
-            } else{
-                minT = midT + 1;
-            }
-            
-        }
-        
         return answer;
     }
 }
