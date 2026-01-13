1a4740079d9835fc631ff98baf53b28e12f016e5
Jihyeon Kim <were1117@sm.ac.kr>
2026-01-13 17:45:40 +0900

    [level 2] Title: 주식가격, Time: 36.80 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,13 +1,25 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
+        int n = prices.length;
         int[] answer = new int[prices.length];
+        Stack<Integer> st = new Stack<>();
         
         for(int i = 0; i < prices.length - 1; i++){
-            for(int j = i + 1; j < prices.length; j++){
-                answer[i]++;
-                if(prices[i] > prices[j]) break;
+            while(!st.isEmpty() && prices[st.peek()] > prices[i]){
+                int temp = st.pop();
+                answer[temp] = i - temp;
             }
+            st.push(i);
+        } 
+        
+        while(!st.isEmpty()){
+            int temp = st.pop();
+            answer[temp] = n - 1 - temp;
         }
+        
+        
         return answer;
     }
 }
