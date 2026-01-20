003699e04934ea4d83f003142e8ad898f95cc0cf
Jihyeon Kim <were1117@sm.ac.kr>
2026-01-20 14:12:33 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 39.91 ms, Memory: 132 MB -BaekjoonHub

---
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -0,0 +1,46 @@
+class Solution {
+    private long getTime(int[] diffs, int[] times, int level){
+        long timeCur = 0;
+        long timePrev = 0;
+        long totalTime = 0;
+        for(int i = 0; i < diffs.length; i++){
+            timeCur = times[i];
+            
+            if (diffs[i] <= level) {
+                totalTime += timeCur;
+            } else {
+                long retry = diffs[i] - level;
+                long prevTime = (i == 0) ? 0 : times[i - 1];
+                totalTime += (prevTime + timeCur) * retry + timeCur;
+            }
+            
+            // 이전 퍼즐의 소요 시간 구하기
+            timePrev += timeCur;
+        }
+        return totalTime;
+    }
+    public int solution(int[] diffs, int[] times, long limit) {
+        int answer = Integer.MAX_VALUE;
+        
+        // 퍼즐 난이도 최댓값 구하기
+        int left = 1;
+        int right = 0;
+        for (int d : diffs) {
+            right = Math.max(right, d);
+        }
+        
+        while(left <= right){
+            int mid = (left + right) / 2;
+            
+            long totalTime = getTime(diffs, times, mid);
+            if (totalTime <= limit) {
+                answer = mid;
+                right = mid - 1; // 더 낮은 level 가능
+            } else {
+                left = mid + 1; // level 더 높여야됨
+            }
+        }
+        
+        return answer;
+    }
+}
