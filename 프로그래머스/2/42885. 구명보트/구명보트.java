526505c1d03d12a5d166ad597ad8fa76aa8d7022
Jihyeon Kim <were1117@sm.ac.kr>
2026-02-19 17:00:14 +0900

    [level 2] Title: 구명보트, Time: 13.39 ms, Memory: 57.9 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -9,15 +9,12 @@ class Solution {
         
         int i = 0;
         int j = N - 1;
-        while(i <= j && j > -1){
+        while(i <= j){
             if(people[i] + people[j] <= limit){
                 i++;
-                j--;
-                answer++;
-            } else {
-                j--;
-                answer++;
             }
+            j--;
+            answer++;
         }
         
         return answer;
