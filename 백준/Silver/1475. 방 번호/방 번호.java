e949245ef2a684b30b036131f6e9ae3b232f72c2
Jihyeon Kim <were1117@sm.ac.kr>
2026-03-02 12:57:08 +0900

    Update 방 번호.java

--- 백준/Silver/1475. 방 번호/방 번호.java
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -1,37 +1,29 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String roomNumber = sc.nextLine();
+        int[] numberCounts = new int[9];
 
-        // 0~9 번호의 int 배열 : 9 안씀 6으로 갱신함
-        int[] arr = new int[9];
-
-        // 방번호 N
-        String doorNum = br.readLine();
-
-        // for N 한자리씩 돌면서
-        for (char num : doorNum.toCharArray()) {
-            // 필요한 번호 체크 : arr[charAt(i)]++
-            // if 필요번호 = 9 면 6++;
-            if (num == '9') {
-                arr[6]++;
-            } else {
-                arr[num - '0']++;
-            }
+        for(char c : roomNumber.toCharArray()){
+            int temp = c - '0';
+            if(temp == 9) numberCounts[6]++;
+            else numberCounts[c - '0']++;
         }
 
-        // 확인하기
-        int count = 0;
-        for (int i = 0; i < 9; i++) {
-            // 필요한번호 for 문 돌면서 최대값 확인 - 8까지 확인
-            if (i == 6) {
-                count = Math.max(count, (arr[i] + 1) / 2);
+        int result = 0;
+
+        for(int i = 0; i < 9; i++){
+            int need = 0;
+            if(i == 6){
+                need = (numberCounts[6] + 1) / 2;;
             } else {
-                count = Math.max(count, arr[i]);
+                need = numberCounts[i];
             }
+            result = Math.max(result, need);
         }
 
-        System.out.println(count);
+        System.out.println(result);
     }
 }
