19f6a34a61d6dcca8c3ec2d435d1a4531ccd6277
Jihyeon Kim <were1117@sm.ac.kr>
2026-03-12 13:35:05 +0900

    [Gold III] Title: 감시, Time: 308 ms, Memory: 124144 KB -BaekjoonHub

---
+++ 백준/Gold/15683. 감시/감시.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int[][] office;
+    private static int[] dx = {1, 0, -1, 0};
+    private static int[] dy = {0, 1, 0, -1};
+    private static int[][][] dir = {
+        {},
+        {{0},{1},{2},{3}},                 // 1
+        {{0,2},{1,3}},                     // 2
+        {{0,1},{1,2},{2,3},{3,0}},         // 3
+        {{0,1,2},{1,2,3},{2,3,0},{3,0,1}}, // 4
+        {{0,1,2,3}}                        // 5
+    };
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        office = new int[N][M];
+        List<int[]> cameraList = new ArrayList<>();
+        int count = 0;
+        for(int i = 0; i < N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0; j < M; j++){
+                office[i][j] = Integer.parseInt(st.nextToken());
+                if(office[i][j] == 0){
+                    count++;
+                }else if(office[i][j] >= 1 && office[i][j]<= 5){
+                    cameraList.add(new int[]{i, j, office[i][j]});
+                }
+            }
+        }
+
+        int result = dfs(cameraList, 0);
+        System.out.println(result);
+    }
+
+    private static int dfs(List<int[]> cameraList, int cameraIndex){
+        int minNum = Integer.MAX_VALUE;
+
+        if(cameraIndex >= cameraList.size()){
+            int count = 0;
+            for(int i=0;i<N;i++){
+                for(int j=0;j<M;j++){
+                    if(office[i][j] == 0)
+                        count++;
+                }
+            }
+            minNum = Math.min(count, minNum);
+            return minNum;
+        }
+
+        int[] camera = cameraList.get(cameraIndex);
+
+        for(int i = 0; i < dir[camera[2]].length; i++) {
+            int[][] copy = new int[N][M];
+            for(int j = 0; j< N; j++){
+                copy[j] = office[j].clone();
+            }
+
+            for(int j = 0; j < dir[camera[2]][i].length; j++){
+                watch(camera[0], camera[1], dir[camera[2]][i][j]);
+            }
+
+            minNum = Math.min(minNum, dfs(cameraList, cameraIndex + 1));
+            
+            for(int j=0; j < N; j++){
+                office[j] = copy[j].clone();
+            }
+        }
+
+        return minNum;
+    }
+
+    private static void watch(int x, int y, int dir){
+        while(true){
+            x += dx[dir];
+            y += dy[dir];
+
+            if(x < 0 || x >= N || y < 0 || y >= M) break;
+            if(office[x][y] == 6) break;
+
+            if(office[x][y] == 0)
+                office[x][y] = -1;
+        }
+    }
+}
