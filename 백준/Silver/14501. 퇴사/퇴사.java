b9e86a884d75b289f72ac189d4c01c51d8d47b1b
Jihyeon Kim <were1117@sm.ac.kr>
2026-04-24 13:33:12 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,36 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int[] dp = new int[N + 1];
-        int[] t = new int[N];
-        int[] p = new int[N];
+        int n = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < N; i++){
-            st = new StringTokenizer(br.readLine());
+        int[] t = new int[n + 1];
+        int[] p = new int[n + 1];
+        int[] dp = new int[n + 2];
+
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             t[i] = Integer.parseInt(st.nextToken());
             p[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(int i = 0; i < N; i++){
-            // 선택 안 함
-            if(i + 1 <= N){
-                dp[i+1] = Math.max(dp[i+1], dp[i]);
-            }
+        // 거꾸로 계산
+        for (int i = n; i >= 0; i--) {
+            int nd = i + t[i];
 
-            // 선택함
-            if(i + t[i] <= N){
-               dp[i + t[i]] = Math.max(dp[i + t[i]], dp[i] + p[i]); 
+            // 이전날꺼 가능함 : 오늘꺼 vs 이전꺼 점수 갱신
+            if (nd <= n + 1) {
+                dp[i] = Math.max(p[i] + dp[nd], dp[i + 1]);
+            } else {
+                // 불가능함 : 오늘거로 함
+                dp[i] = dp[i + 1];
             }
         }
 
-        System.out.print(dp[N]);
+        System.out.println(dp[1]);
     }
 }
