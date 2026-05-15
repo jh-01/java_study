dec13c34d5f12e982b041ca655d5aeb56793d58c
Jihyeon Kim <were1117@sm.ac.kr>
2026-05-15 23:29:46 +0900

    [level 3] Title: 섬 연결하기, Time: 0.51 ms, Memory: 62.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,7 +1,28 @@
 import java.util.*;
 
 class Solution {
-    private int[] parent;
+    int[] parent;
+    
+    public int solution(int n, int[][] costs) {
+        int cost = 0;       
+        Arrays.sort(costs, (a, b) -> {return a[2] - b[2];});
+        
+        parent = new int[n];
+        for(int i = 0; i < n; i++){
+            parent[i] = i;
+        }
+        
+        for(int i = 0; i < costs.length; i++){
+            int[] temp = costs[i];
+            int a = temp[0];
+            int b = temp[1];
+            
+            if(union(a, b)){
+                cost += temp[2];
+            }
+        }
+        return cost;
+    }
     
     private int find(int x){
         if(parent[x] != x) {
@@ -17,30 +38,5 @@ class Solution {
         if(x == y) return false; // 이미 연결됨
         parent[y] = x;
         return true;
-    }
-    
-    public int solution(int n, int[][] costs) {
-        int answer = 0;
-        
-        // cost 기준으로 정렬
-        Arrays.sort(costs, (a, b) -> a[2] - b[2]);
-        
-        // 리스트에 값 넣기
-        parent = new int[n];
-        for(int i = 0; i < n; i++){
-            parent[i] = i;
-        }
-        
-        for(int i = 0; i < costs.length; i++){
-            int x = costs[i][0];
-            int y = costs[i][1];
-            int cost = costs[i][2];
-            
-            // 연결됐는지 확인
-            if(union(x, y)){
-                answer += cost;
-            }
-        }
-        return answer;
     }
 }
