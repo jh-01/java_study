281352a14d523cfdbbcc8c6fa9b0c6e28dd400a4
Jihyeon Kim <were1117@sm.ac.kr>
2026-08-04 10:23:47 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.41 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -1,21 +1,24 @@
-import java.util.stream.*;
-
 class Solution {
     public int[] solution(int[] arr) {
-        int[] answer = null;
-        if(arr.length == 1) answer = new int[] {-1};
-        else {
-            int minNum = Integer.MAX_VALUE;
-            int index = -1;
-            for(int i = 0; i < arr.length; i++){
-                if(arr[i] < minNum) {
-                    minNum = arr[i];
-                    index = i;
-                }
-            }
-            final int min = minNum;
-            answer = IntStream.of(arr).filter(num -> num != min).toArray();
+        int[] answer = {};
+        
+        if(arr.length <= 1){
+            answer = new int[]{-1};
+            return answer;
         }
+        
+        int minN = Integer.MAX_VALUE;
+        answer = new int[arr.length - 1];
+        for(int i = 0; i < arr.length; i++){
+            minN = Math.min(minN, arr[i]);
+        }
+        int j = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] != minN){
+                answer[j++] = arr[i];
+            }
+        }
+        
         return answer;
     }
 }
